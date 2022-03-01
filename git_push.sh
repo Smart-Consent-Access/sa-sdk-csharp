74eb41887d0bc18fe93101e573cbdb6764716e9c
luso <lukas.solberg@knightec.se>
2022-03-01 09:26:36 +0100

    update

--- git_push.sh
+++ git_push.sh
@@ -4,31 +4,26 @@
 # Usage example: /bin/sh ./git_push.sh "minor update"
 
 release_note=$1
-
 git_repo_id=Smart-Consent-Access/sa-sdk-csharp
+
+# Creates a temporary folder and clone the SDK repo into it
+mkdir temp
+(cd temp && git clone git@github.com:$git_repo_id.git)
+
+# Copy the fils over to the temporary folder
+rsync -rv --exclude=temp . temp/sa-sdk-csharp/
 
 if [ "$release_note" = "" ]; then
     release_note="Minor update"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
-# Initialize the local directory as a Git repository
-git init
-
-# Adds the files in the local repository and stages them for commit.
-git add .
-
-# Commits the tracked changes and prepares them to be pushed to a remote repository. 
-git commit -m "$release_note"
-
-# Sets the new remote
-git_remote=`git remote`
-if [ "$git_remote" = "" ]; then # git remote not defined
-    git remote add origin git@github.com:$git_repo_id.git
-fi
-
-git pull origin master
+# Adds the files and commit them.
+cd temp/sa-sdk-csharp && git add . && git commit -m "$release_note"
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to git@github.com:$git_repo_id.git"
 git push origin master 2>&1 | grep -v 'To https'
+
+# Remove the temporary folder
+cd ../../ && rm -rf temp/
